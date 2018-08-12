5c05822ed8a7bca0c24e36bf8f82f734551ff595
Timo Aho <zeukkari@gmail.com>
2018-08-12 22:46:05 +0300

    A Pong Game

--- src/key.js
+++ src/key.js
@@ -1,28 +1,36 @@
 var _pressed = {};
 var key = {};
 
-key.LEFT = 37;
 key.UP = 38;
-key.RIGHT = 39;
 key.DOWN = 40;
+key.W = 87;
+key.S = 83;
 
-key.isDown = function (keyCode) {
+key.isDown = function(keyCode) {
   return _pressed[keyCode];
 };
 
-key.onKeydown = function (event) {
+key.onKeydown = function(event) {
   _pressed[event.keyCode] = true;
 };
 
-key.onKeyup = function  (event) {
+key.onKeyup = function(event) {
   _pressed[event.keyCode] = null;
 };
 
-window.addEventListener('keyup', function (e) {
-  key.onKeyup(e);
-}, false);
-window.addEventListener('keydown', function (e) {
-  key.onKeydown(e);
-}, false);
+window.addEventListener(
+  'keyup',
+  function(e) {
+    key.onKeyup(e);
+  },
+  false
+);
+window.addEventListener(
+  'keydown',
+  function(e) {
+    key.onKeydown(e);
+  },
+  false
+);
 
 module.exports = key;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var loop = require('./loop');
 var rand = require('./rand');
 var key = require('./key');
+var math = require('mathjs');
 
 var canvas = document.createElement('canvas');
 canvas.width = 640;
@@ -10,49 +11,198 @@ document.body.appendChild(canvas);
 
 var ctx = canvas.getContext('2d');
 
-// demo entity
-var mob = {
+var gameState = {
+  player1Score: 0,
+  player2Score: 0
+};
+
+var player1 = {
   x: rand.int(canvas.width),
   y: rand.int(canvas.height),
   width: 25,
-  height: 25,
-  speed: 150,
+  height: 120,
+  speed: 350,
   color: 'rgba(236, 94, 103, 1)'
 };
 
-// game loop
-loop.start(function (dt) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+var player2 = {
+  x: rand.int(canvas.width),
+  y: rand.int(canvas.height),
+  width: 25,
+  height: 120,
+  speed: 350,
+  color: 'rgba(36, 94, 103, 1)'
+};
 
-  // update mob
-  if (key.isDown(key.LEFT)) {
-    mob.x = mob.x - (mob.speed * dt);
+var upperWall = {
+  x: 0,
+  y: 0,
+  width: canvas.width,
+  height: 25,
+  color: 'rgba(0, 0, 255, 1)'
+};
+
+var lowerWall = {
+  x: 0,
+  y: canvas.height - 25,
+  width: canvas.width,
+  height: 25,
+  color: 'rgba(0, 0, 255, 1)'
+};
+
+var ball = {
+  x: parseInt(canvas.width / 2),
+  y: parseInt(canvas.height / 2),
+  radius: 25,
+  speed: 350,
+  direction: [Math.random(), Math.random()],
+  color: 'rgba(255, 255, 0, 1)'
+};
+
+var RectCircleColliding = function(circle, rect) {
+  var distX = Math.abs(circle.x - rect.x - rect.width / 2);
+  var distY = Math.abs(circle.y - rect.y - rect.height / 2);
+
+  if (distX > rect.width / 2 + circle.radius) {
+    return false;
   }
-  if (key.isDown(key.RIGHT)) {
-    mob.x = mob.x + (mob.speed * dt);
+  if (distY > rect.height / 2 + circle.radius) {
+    return false;
   }
+
+  if (distX <= rect.width / 2) {
+    return true;
+  }
+  if (distY <= rect.height / 2) {
+    return true;
+  }
+
+  var dx = distX - rect.width / 2;
+  var dy = distY - rect.height / 2;
+  return dx * dx + dy * dy <= circle.radius * circle.radius;
+};
+
+var player1Movement = function(dt) {
+  if (key.isDown(key.W)) {
+    player1.y = player1.y - player1.speed * dt;
+  }
+  if (key.isDown(key.S)) {
+    player1.y = player1.y + player1.speed * dt;
+  }
+
+  if (player1.y < 0) {
+    player1.y = 0;
+  } else if (player1.y + player1.height > canvas.height) {
+    player1.y = canvas.height - player1.height;
+  }
+};
+
+var player2Movement = function(dt) {
   if (key.isDown(key.UP)) {
-    mob.y = mob.y - (mob.speed * dt);
+    player2.y = player2.y - player2.speed * dt;
   }
   if (key.isDown(key.DOWN)) {
-    mob.y = mob.y + (mob.speed * dt);
+    player2.y = player2.y + player2.speed * dt;
   }
+
+  if (player2.y < 0) {
+    player2.y = 0;
+  } else if (player2.y + player2.height > canvas.height) {
+    player2.y = canvas.height - player2.height;
+  }
+};
+
+var pongMovement = function(dt) {
+  ball.x = ball.x + ball.direction[0] * ball.speed * dt;
+  ball.y = ball.y + ball.direction[1] * ball.speed * dt;
+
+  var normalTop = [0, -1];
+  var normalBottom = [0, 1];
+  var normalLeft = [-1, 0];
+  var normalRight = [1, 0];
+
+  var normal;
 
   // check bounds collisions
-  if (mob.x < 0) {
-    mob.x = canvas.width;
-  } else if (mob.x > canvas.width) {
-    mob.x = 0;
-  }
-  if (mob.y < 0) {
-    mob.y = canvas.height;
-  } else if (mob.y > canvas.height) {
-    mob.y = 0;
+  if (RectCircleColliding(ball, player1)) {
+    ball.speed = ball.speed * 1.1;
+    player1.height = player1.height * 0.9;
+    normal = normalLeft;
   }
 
-  // draw mob
-  ctx.fillStyle = mob.color;
-  ctx.fillRect(mob.x, mob.y, mob.width, mob.height);
+  if (RectCircleColliding(ball, player2)) {
+    ball.speed = ball.speed * 1.1;
+    player2.height = player2.height * 0.9;
+    normal = normalRight;
+  }
+  if (RectCircleColliding(ball, lowerWall)) {
+    normal = normalBottom;
+  }
+  if (RectCircleColliding(ball, upperWall)) {
+    normal = normalTop;
+  }
 
-  console.log('game update fn %s', dt);
+  if (normal) {
+    var temp = 2 * math.dot(ball.direction, normal);
+    var temp2 = math.multiply(temp, normal);
+    var newDirection = math.subtract(ball.direction, temp2);
+    ball.direction = newDirection;
+  }
+};
+
+var resetBall = function() {
+  ball.x = parseInt(canvas.width / 2);
+  ball.y = parseInt(canvas.height / 2);
+  ball.direction = [Math.random(), Math.random()];
+  ball.speed = 350;
+};
+
+var testScore = function() {
+  if (ball.x < 0) {
+    gameState.player1Score++;
+    player1.height = 120;
+    resetBall();
+  }
+  if (ball.x > canvas.width) {
+    gameState.player2Score++;
+    player2.height = 120;
+    resetBall();
+  }
+};
+
+// game loop
+loop.start(function(dt) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  player1Movement(dt);
+  player2Movement(dt);
+  pongMovement(dt);
+  testScore();
+
+  // draw walls
+  ctx.fillStyle = upperWall.color;
+  ctx.fillRect(upperWall.x, upperWall.y, upperWall.width, upperWall.height);
+  ctx.fillStyle = lowerWall.color;
+  ctx.fillRect(lowerWall.x, lowerWall.y, lowerWall.width, lowerWall.height);
+
+  // draw player1
+  ctx.fillStyle = player1.color;
+  ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+  // draw player2
+  ctx.fillStyle = player2.color;
+  ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+
+  // draw ball
+  ctx.fillStyle = ball.color;
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI, false);
+  ctx.fillStyle = ball.color;
+  ctx.fill();
+
+  // draw score
+  ctx.font = '30px Georgia';
+  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+  var scoreText = `${gameState.player1Score} - ${gameState.player2Score}`;
+  ctx.fillText(scoreText, canvas.width / 2, canvas.height * (1 / 4));
 });
